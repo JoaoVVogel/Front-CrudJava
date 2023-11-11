0e6bdfaf9b7b2fd08dd92e3ed3628651fe6cf902
João Vitor Vogel Vieira <joao0404200@gmail.com>
2023-11-10 21:57:54 -0300

    api funcionando

--- js/curso.js
+++ js/curso.js
@@ -2,7 +2,7 @@ const endpointCurso = "http://18.227.26.15:8080/api/curso";
 
 async function cursos(){
     const result = await fetch(endpointCurso, {
-        method: "GET",
+        method: "GET"
     });
 
     const data = await result.json();
@@ -10,14 +10,21 @@ async function cursos(){
     console.log(data)
 
     const cardCursos = document.getElementById("card-cursos");
-    const cursosInfo = data.result;
+    const cursosInfo = data;
 
     cursosInfo.forEach(curso => {
         const card = document.createElement("div");
-        card.classList.add("card");
+        card.classList.add("card-curso");
 
         const nomeCurso = document.createElement("h1");
-        nomeCurso.innerText=curso.nome
+        nomeCurso.classList.add("titulo-curso");
+        nomeCurso.innerText=curso.nome;
+
+        const tipoCurso = document.createElement("h2");
+        tipoCurso.classList.add("tipo-curso");
+        tipoCurso.innerText=curso.titulo;
+
+        card.appendChild(nomeCurso)
     
         cardCursos.appendChild(card);
     });
