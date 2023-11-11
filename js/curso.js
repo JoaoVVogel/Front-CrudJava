e8600f24c26ddc1232ea81fd0923ef0f0c151b07
João Vitor Vogel Vieira <joao0404200@gmail.com>
2023-11-10 21:31:49 -0300

    inicio do consumo de apis

--- js/curso.js
+++ js/curso.js
@@ -1,1 +1,25 @@
-const endpointCurso = "";
+const endpointCurso = "http://18.227.26.15:8080/api/curso";
+
+async function cursos(){
+    const result = await fetch(endpointCurso, {
+        method: "GET",
+    });
+
+    const data = await result.json();
+    console.log(result)
+    console.log(data)
+
+    const cardCursos = document.getElementById("card-cursos");
+    const cursosInfo = data.result;
+
+    cursosInfo.forEach(curso => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const nomeCurso = document.createElement("h1");
+        nomeCurso.innerText=curso.nome
+    
+        cardCursos.appendChild(card);
+    });
+}
+cursos();
